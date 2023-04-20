3524c17cd754d6f3b591be76a7e7b7d3d115efef
minhaj539 <115364661+minhaj539@users.noreply.github.com>
2023-04-20 20:07:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 //your JS code here. If required.
-const date=new Date();
-document.write(date.toLocaleString());
+
+const p=document.getElementById("timer");
+window.onload = displayClock();
+function displayClock(){
+  var display = new Date().toLocaleString();
+	p.innerText=display;
+  // document.body.innerHTML = display;
+  setTimeout(displayClock, 1000); 
+}
